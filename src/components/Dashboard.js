a9cd59fe8d55b89d109c3a95e9b46ec90cb6b3b7
snallamada <shravanreddynallamada@gmail.com>
2022-08-31 11:03:33 +0200

    test

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Dashboard = () => {
   return (
-    <div>Testing Dashboard</div>
+    <div><center><b>Dashboard</b></center></div>
   )
 }
 
---
+++ src/components/Logout.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Logout = () => {
+  return (
+    <div><center><b>Logout</b></center></div>
+  )
+}
+
+export default Logout
